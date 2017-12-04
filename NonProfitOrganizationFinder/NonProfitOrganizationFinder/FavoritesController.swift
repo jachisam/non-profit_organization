d760c6cc2357a954dcb10bc91b959a38648a720b
Oscar <oariasdev@gmail.com>
2017-12-03 21:09:37 -0600

    faves

--- NonProfitOrganizationFinder/NonProfitOrganizationFinder/FavoritesController.swift
+++ NonProfitOrganizationFinder/NonProfitOrganizationFinder/FavoritesController.swift
@@ -9,182 +9,72 @@
 import Foundation
 import UIKit
 
-class FavoritesViewController: UIViewController{
-//class FavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class FavesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var nonProfitNames: [String] = []
     
-    @IBOutlet weak var tableView: UITableView!
-    
-    /*
-    
-    //DATA
-    var theData: [NonProfit] = []
-    let storage = UserDefaults.standard.array(forKey: "nonprofits") as! [String] //storage ...
-    
-    // https://stackoverflow.com/questions/25392124/swift-func-viewwillappear
-    // needed in order to refresh favourites list during same app launch
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        refreshFaves()
-    }
-    
-    // update the favourites between tabs
-    private func refreshFaves() {
-        DispatchQueue.global(qos: .userInitiated).async {
-            self.fetchFavourites()
-            
-            DispatchQueue.main.async {
-                self.tableView.reloadData()
-            }
-        }
-    }
-    
-    // fetch data for the favourites
-    private func fetchFavourites() {
-    
-        theData = []
-        
-        for item in storage {
-            //change to our website and pull by name
-            let apiKey = "43837a3ce3a42866abb9ead7c3de29fa"
-            let json = getJSON("https://api.themoviedb.org/3/movie/"+item+"?api_key="+apiKey)
-            
-        }
-    }
-    
-    // (todd sproull)
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let nonprofFav:NonProfit = theData[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCellFav", for: indexPath) as! MyTableViewCell
-      
-        cell.label?.text = nonprofFav.name
-        return cell
-    }
-    
-    // num rows in section (todd sproull)
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return theData.count
-        
-    }
-    
-    // Open information for movie (todd sproull)
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let movie:NonProfit = theData[indexPath.row]
-        let detailedVC = storyboard!.instantiateViewController(withIdentifier: "OrganizationPage") as! DetailedViewController //DetailedViewController()
-        
-        //detailedVC.title = nonprofFav.name
-        //detailedVC.name = movie.movieID
-        navigationController?.pushViewController(detailedVC, animated: true)
-        
-    }
-    
-    // https://code.tutsplus.com/tutorials/working-with-icloud-key-value-storage--pre-37542
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if (editingStyle == UITableViewCellEditingStyle.delete) {
-           // var array = storage.array(forKey: "nonprofits") as? [String]
-      //      array.remove(at: indexPath.row)
-       //     storage.set(array, forKey: "nonprofits")
-       //     refreshFaves()
-        }
-    }
-    
-    //setup TableView (todd sproull)
-    func setupTableView() {
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-    }
-    
-    private func getJSON(_ url:String) {
-        let username = "80f27268088b460b9138a26c44e3266c"
-        let password = ""
-        let credentials = String(format: "%@:%@", username, password).data(using: String.Encoding.utf8)!
-        let base64LoginData = credentials.base64EncodedString()
-        
-        let url = URL(string: url)!
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
-        
-       // dispatchGroup.enter()
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let data = data {
-                if let jsonFavData = try? JSON(data: data) {
-                    for result in jsonFavData["hits"].arrayValue {
-                        let id = result["organization_id"].stringValue
-                        let detailUrl = "https://sandboxdata.guidestar.org/v1/detail/\(id).json"
-                        //print(id)
-                        self.getDetailJSON(detailUrl)
-                    }
-                    //self.dispatchGroup.leave()
-                }
-                else {
-                    print("No Search JSON Data")
-                }
-            }
-        }
-        task.resume()
-    }
-    
-    private func getDetailJSON(_ url:String) {
-        let username = "a45b1421439743eb970b0f1bef3133e8"
-        let password = ""
-        let credentials = String(format: "%@:%@", username, password).data(using: String.Encoding.utf8)!
-        let base64LoginData = credentials.base64EncodedString()
-        
-        let url = URL(string: url)!
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue("Basic \(base64LoginData)", forHTTPHeaderField: "Authorization")
-        
-        //dispatchGroup.enter()
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let data = data {
-                if let jsonFavData = try? JSON(data: data) {
-                    let name = jsonFavData["organization_name"].stringValue
-                    let mission = jsonFavData["mission"].stringValue
-                    let affiliationCode = jsonFavData["affilitation_code"].stringValue
-                    let address = jsonFavData["address_line1"].stringValue
-                    let city = jsonFavData["city"].stringValue
-                    let state = jsonFavData["state"].stringValue
-                    let zip = jsonFavData["zip"].stringValue
-                    let telephone = jsonFavData["telephone"].stringValue
-                    let website = jsonFavData["website"].stringValue
-                    let id = jsonFavData["organization_id"].stringValue
-                    
-                    let nonprofit = NonProfit(name: name, mission: mission, affilitationCode: affiliationCode, address: address, city: city, state: state, zip: zip, telephone: telephone, websiteURL: website, id: id)
-                    
-                    //print(nonprofit.name)
-                    self.theData.append(nonprofit)
-                }
-                else {
-                    print("No Detail JSON Data")
-                }
-            }
-      //      self.dispatchGroup.leave()
-        }
-        task.resume()
-    }
-    
-    // used (todd sproull) then updated image cache to suuport dictionary use
+    @IBOutlet weak var favesTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTableView()
+        
+        // Do any additional setup after loading the view.
+        
+        favesTable.dataSource = self
+        favesTable.delegate = self
+        favesTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-   
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    override func viewWillAppear(_ animated: Bool) {
+        nonProfitNames = []
+        let defaults = UserDefaults.standard
+        if let temp = defaults.array(forKey: "nonprofits") {
+            for nonProfit in temp {
+                nonProfitNames.append(nonProfit as! String)
+            }
+        }
+        else {
+            defaults.set([String](), forKey: "nonprofits")
+        }
+        print(nonProfitNames)
+        favesTable.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.adjustsFontSizeToFitWidth = true
+        cell.textLabel!.text = nonProfitNames[indexPath.row]
+        return cell
         
     }
- */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nonProfitNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            nonProfitNames.remove(at: indexPath.row)
+            
+            let defaults = UserDefaults.standard
+            defaults.set(nonProfitNames, forKey: "nonprofits")
+            favesTable.reloadData()
+        }
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
-
